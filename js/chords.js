8cceecd97359cfe3e02ff5b54dfcb255326ad312
Lucas Vance <lucas.vance@protonmail.com>
2022-09-19 15:40:09 +0000

    Consolidate chord generator

--- js/chords.js
+++ js/chords.js
@@ -1,25 +1,21 @@
-// This will be the script that generates the chords and exports an array of objects
+function generateChords(bars) {
+    let chords = [];
+    let totalBeats = 0;
+    const targetBeats = bars * 4;
 
-let chords = [];
-chords.push({ chord: 1, beats: 4 });
+    while (totalBeats < targetBeats) {
+        console.log("totalBeats: " + totalBeats);
+        chord = Math.floor(Math.random() * 8);
 
-function generateFactorOf(num) {
-    // Declare minimum and maximum values; maximum is 3 because I will be using this as a factor of 2, and 2^3 = 8, which is the real max number that I want.
-    const min = 0;
-    const max = 4;
+        beats = 2 ** Math.floor(Math.random() * 4);
+        console.log("beats: " + beats);
+        if (beats <= targetBeats - totalBeats) {
+            totalBeats += beats;
+            chords.push({ chord: chord, beats: beats });
+        }
+    }
 
-    // Generate number between 0 and 3
-    return num ** Math.floor(Math.random() * (max - min) + min);
+    return chords;
 }
 
-const bars = 8;
-let totalBeats = 0;
-
-while (totalBeats < bars * 4) {
-    chord = Math.floor(Math.random() * 8);
-    beats = generateFactorOf(2);
-    totalBeats += beats;
-    chords.push({ chord: chord, beats: beats });
-}
-
-console.log(chords);
+console.log(generateChords(8));
