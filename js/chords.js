0b1da6656168564e88d1e53857b9ab951a5f2a51
Lucas Vance <lucas.vance@protonmail.com>
2022-09-19 15:47:34 +0000

    Added chords as a module export

--- js/chords.js
+++ js/chords.js
@@ -1,14 +1,16 @@
 function generateChords(bars) {
+    // Define empty chord array, totalBeats, and targetBeats
     let chords = [];
     let totalBeats = 0;
+    // Since we are assuming the generated progression will be in 4/4, each bar is worth 4 beats
     const targetBeats = bars * 4;
 
+    // While totalBeats is not up to targetBeats, generate random chords
     while (totalBeats < targetBeats) {
-        console.log("totalBeats: " + totalBeats);
         chord = Math.floor(Math.random() * 8);
-
         beats = 2 ** Math.floor(Math.random() * 4);
-        console.log("beats: " + beats);
+
+        // Only push to the chords array if the new amount of beats won't overflow the targetBeats
         if (beats <= targetBeats - totalBeats) {
             totalBeats += beats;
             chords.push({ chord: chord, beats: beats });
@@ -18,4 +20,5 @@ function generateChords(bars) {
     return chords;
 }
 
-console.log(generateChords(8));
+// export module
+module.exports = { generateChords };
