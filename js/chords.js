2a2b3a8e9e93b15dc6e2a3c842ef4fa1476d4b5a
Lucas Vance <lucas.vance@protonmail.com>
2022-09-21 15:12:00 +0000

    Updated formatting to use two spaces for tabs

--- js/chords.js
+++ js/chords.js
@@ -1,21 +1,21 @@
 export function generateChords(bars) {
-    // Define empty chord array, totalBeats, and targetBeats
-    let chords = [];
-    let totalBeats = 0;
-    // Since we are assuming the generated progression will be in 4/4, each bar is worth 4 beats
-    const targetBeats = bars * 4;
+  // Define empty chord array, totalBeats, and targetBeats
+  let chords = [];
+  let totalBeats = 0;
+  // Since we are assuming the generated progression will be in 4/4, each bar is worth 4 beats
+  const targetBeats = bars * 4;
 
-    // While totalBeats is not up to targetBeats, generate random chords
-    while (totalBeats < targetBeats) {
-        const chord = Math.floor(Math.random() * 8);
-        const beats = 2 ** Math.floor(Math.random() * 4);
+  // While totalBeats is not up to targetBeats, generate random chords
+  while (totalBeats < targetBeats) {
+    const chord = Math.floor(Math.random() * 8);
+    const beats = 2 ** Math.floor(Math.random() * 4);
 
-        // Only push to the chords array if the new amount of beats won't overflow the targetBeats
-        if (beats <= targetBeats - totalBeats) {
-            totalBeats += beats;
-            chords.push({ chord: chord, beats: beats });
-        }
+    // Only push to the chords array if the new amount of beats won't overflow the targetBeats
+    if (beats <= targetBeats - totalBeats) {
+      totalBeats += beats;
+      chords.push({ chord: chord, beats: beats });
     }
+  }
 
-    return chords;
+  return chords;
 }
