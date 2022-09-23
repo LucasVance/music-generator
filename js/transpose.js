1f2f4bec834502ae163e4ed217009a08e92bc4dc
Lucas Vance <lucas.vance@protonmail.com>
2022-09-23 18:07:42 +0000

    Add transpose.js

--- js/transpose.js
+++ js/transpose.js
@@ -1,5 +1,5 @@
-function transpose(note) {
-  keys = {
+export function transpose(note, key) {
+  const keys = {
     c: ["C", "D", "E", "F", "G", "A", "B"],
     cs: ["C#", "D#", "F", "F#", "G#", "A#", "C"],
     d: ["D", "E", "F#", "G", "A", "B", "C#"],
@@ -13,4 +13,46 @@ function transpose(note) {
     as: ["A", "A#", "C", "D", "D#", "F", "G", "A"],
     b: ["B", "C#", "D#", "E", "F#", "G#", "A#"],
   };
+
+  switch (key) {
+    case "c":
+      key = keys.c;
+      break;
+    case "cs":
+      key = keys.cs;
+      break;
+    case "d":
+      key = keys.d;
+      break;
+    case "ds":
+      key = keys.ds;
+      break;
+    case "e":
+      key = keys.e;
+      break;
+    case "f":
+      key = keys.f;
+      break;
+    case "g":
+      key = keys.g;
+      break;
+    case "gs":
+      key = keys.gs;
+      break;
+    case "a":
+      key = keys.a;
+      break;
+    case "as":
+      key = keys.as;
+      break;
+    case "b":
+      key = keys.b;
+      break;
+
+    default:
+      return "Please enter a valid key.";
+      break;
+  }
+
+  return key[note - 1];
 }
