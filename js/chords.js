bddc258d90b7df71c978a2e35ddfb1cab23a11da
Lucas Vance <lucas.vance@protonmail.com>
2022-09-23 23:11:52 +0000

    Removed automatic formatting in chords.js

--- js/chords.js
+++ js/chords.js
@@ -2,9 +2,6 @@ export function generateChords(bars) {
   // Define empty chord array, totalBeats, and targetBeats
   let chords = [];
   let totalBeats = 0;
-  let result = "";
-  let resultChords = [];
-  let resultBeats = [];
   // Since we are assuming the generated progression will be in 4/4, each bar is worth 4 beats
   const targetBeats = bars * 4;
 
@@ -20,14 +17,7 @@ export function generateChords(bars) {
     }
   }
 
-  for (let i = 0; i < chords.length; i++) {
-    // resultChords.push(chords[i].chord);
-    // resultBeats.push(chords[i].beats);
-
-    result += `Play chord ${chords[i].chord} for ${chords[i].beats} beats \n`;
-  }
-
-  return result;
+  return chords;
 }
 
 console.log(generateChords(8));
