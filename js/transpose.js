e61cf36a45fcf664fe02176600f8c5bbaedfd440
Lucas Vance <lucas.vance@protonmail.com>
2023-03-03 17:45:53 -0800

    Massively simplify transpose.js

--- js/transpose.js
+++ js/transpose.js
@@ -1,59 +1,28 @@
-export function transpose(note, currentKey) {
+function transpose(note, currentKey) {
     // List of keys as an object consisting of named arrays
     const keys = {
-        c: ["C", "D", "E", "F", "G", "A", "B"],
-        cs: ["C#", "D#", "F", "F#", "G#", "A#", "C"],
-        d: ["D", "E", "F#", "G", "A", "B", "C#"],
-        ds: ["D#", "F", "G", "G#", "A#", "C", "D"],
-        e: ["E", "F#", "G#", "A", "B", "C#", "D#"],
-        f: ["F", "G", "A", "A#", "C", "D", "E"],
-        fs: ["F#", "G#", "A#", "B", "C#", "D#", "F"],
-        g: ["G", "A", "B", "C", "D", "E", "F#"],
-        gs: ["G#", "A#", "C", "C#", "D#", "F", "G"],
-        a: ["A", "B", "C#", "D", "E", "F#", "G#"],
-        as: ["A", "A#", "C", "D", "D#", "F", "G", "A"],
-        b: ["B", "C#", "D#", "E", "F#", "G#", "A#"],
+        C: ["nothing", "C", "D", "E", "F", "G", "A", "B"],
+        "C#": ["nothing", "C#", "D#", "F", "F#", "G#", "A#", "C"],
+        D: ["nothing", "D", "E", "F#", "G", "A", "B", "C#"],
+        "D#": ["nothing", "D#", "F", "G", "G#", "A#", "C", "D"],
+        E: ["nothing", "E", "F#", "G#", "A", "B", "C#", "D#"],
+        F: ["nothing", "F", "G", "A", "A#", "C", "D", "E"],
+        "F#": ["nothing", "F#", "G#", "A#", "B", "C#", "D#", "F"],
+        G: ["nothing", "G", "A", "B", "C", "D", "E", "F#"],
+        "G#": ["nothing", "G#", "A#", "C", "C#", "D#", "F", "G"],
+        A: ["nothing", "A", "B", "C#", "D", "E", "F#", "G#"],
+        "A#": ["nothing", "A#", "C", "D", "D#", "F", "G", "A"],
+        B: ["nothing", "B", "C#", "D#", "E", "F#", "G#", "A#"],
     };
 
-    // Set current key
-    switch (currentKey) {
-        case "c":
-            currentKey = keys.c;
-            break;
-        case "cs":
-            currentKey = keys.cs;
-            break;
-        case "d":
-            currentKey = keys.d;
-            break;
-        case "ds":
-            currentKey = keys.ds;
-            break;
-        case "e":
-            currentKey = keys.e;
-            break;
-        case "f":
-            currentKey = keys.f;
-            break;
-        case "g":
-            currentKey = keys.g;
-            break;
-        case "gs":
-            currentKey = keys.gs;
-            break;
-        case "a":
-            currentKey = keys.a;
-            break;
-        case "as":
-            currentKey = keys.as;
-            break;
-        case "b":
-            currentKey = keys.b;
-            break;
-
-        default:
-            return "Please enter a valid key.";
+    // Check if currentKey exists in the keys object
+    if (!keys.hasOwnProperty(currentKey)) {
+        return "Please enter a valid key.";
     }
 
-    return currentKey[note - 1];
+    // Map the currentKey input to the corresponding array in the keys object
+    const keyArray = keys[currentKey];
+
+    // Return the note at the index of note - 1 in the keyArray
+    return keyArray[note];
 }
