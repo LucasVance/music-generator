b928bbc6fb9f32d757a32b9f3e84f1b3768534d3
Lucas Vance <lucas.vance@protonmail.com>
2023-03-03 18:21:10 -0800

    Update index.js to include transposition of notes

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+// Import modules from /js folder
 import { generateChords } from "./js/chords.js";
 import { transpose } from "./js/transpose.js";
 
+// Declare page elements
 const submitBtn = document.getElementById("submitBtn");
 const bars = document.getElementById("bars");
 const resultBox = document.getElementById("result");
-const key = document.querySelector('input[name="key"]:checked');
 
+// On click
 submitBtn.addEventListener("click", () => {
+    // Get current key from radio buttons
+    const key = document.querySelector('input[name="key"]:checked').value;
+    // Generate array of chords
+    const chords = generateChords(bars.value);
+    console.log(chords);
+    // Declare empty string that will be added to
     let output = "";
-    let chords = generateChords(bars.value);
+
+    // Loop until chords is filled according to amount of bars inputted by user
     for (let i = 0; i < chords.length; i++) {
-        output += `Play ${chords[i].chord} for ${chords[i].beats} beats \n`;
+        // Add string to output with newline
+        output += `Play ${transpose(chords[i].chord, key)} for ${
+            chords[i].beats
+        } beats \n`;
     }
-    console.log(key);
+    // Set the result box to the output
     resultBox.innerText = output;
 });
